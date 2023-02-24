bc034c57e4fe17392e782f11ecf82014726d06ac
Bruno-GSilva <gisellenanda-@hotmail.com>
2023-02-24 12:06:24 -0300

    aula19

---
+++ eventos.js
@@ -0,0 +1,9 @@
+function selecionar (input) {
+    quantidade.innerHTML = "1";
+    if (input.checked) {
+        quantidade.innerHTML = "1";
+    } else {
+        quantidade.innerHTML = "0";
+
+    }
+}
